b7c1fa1bce7b7e3e8fa85108fe31bab2a931d364
natalia6464 <85963427+natalia6464@users.noreply.github.com>
2021-08-27 18:46:18 +0300

    Update day-03.js

--- day-03.js
+++ day-03.js
@@ -1,8 +1,9 @@
+//http://www.codewars.com/kata/571f832f07363d295d001ba8
 
-
-
-
-
+function trueOrFalse(val){
+  if (val == false || val == null || val == undefined)  return "false";             
+  else     return "true";
+}
 
 
 //http://www.codewars.com/kata/57202aefe8d6c514300001fd
