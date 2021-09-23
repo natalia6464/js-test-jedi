3f8a2d8ceb205a96b26d073201ba96d6af44ad73
natalia6464 <85963427+natalia6464@users.noreply.github.com>
2021-09-23 23:59:28 +0300

    Update day-04.js

--- day-04.js
+++ day-04.js
@@ -1,1 +1,31 @@
 //
+function padIt(str,n){
+  const starsL = []; 
+  const starsR = [];
+    if( n % 2 === 0){
+  let i = 0;
+  while(i < n / 2){
+    starsL[i] = "*";
+    i++;
+  };
+  let j = 0;
+  while ( j < n / 2){
+    starsR[j] = "*";
+    j++
+  };
+  }
+  else{
+  let i = 0;
+  while( i < n - (n / 2)){
+    starsL[i] = "*";
+    i++;
+  };
+  let j = 0;
+  while ( j < n / 2 - 1){
+    starsR[j] = "*";
+    j++
+  };
+  };
+   let starsAndStr = starsL.join("") + str + starsR.join("");
+      return starsAndStr;
+}
